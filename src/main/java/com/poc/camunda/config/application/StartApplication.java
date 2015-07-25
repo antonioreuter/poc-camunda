d2a0c723f296d7225a93fcfa72ebc3f6313937f9
Antonio Reuter <antonioreuter@gmail.com>
2015-07-24 22:53:08 -0300

    first commit

---
+++ src/main/java/com/poc/camunda/config/application/StartApplication.java
@@ -0,0 +1,12 @@
+package com.poc.camunda.config.application;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by antonioreuter on 7/24/15.
+ */
+@SpringBootApplication
+public class StartApplication {
+
+
+}
